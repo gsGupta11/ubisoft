a31762480d9ccead98d5a8d0aac0fedfdedc53da
prince300 <princehans200@gmail.com>
2020-10-12 16:30:06 +0530

    first

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 require('dotenv').config()
 makeConnection = () => {
-    s="mongodb+srv://"+process.env.USERNAME+":"+process.env.PASS+"@cluster0.wveny.mongodb.net/ubisoft?retryWrites=true&w=majority"
-    mongoose.connect(s, {
+    // s="mongodb+srv://"+process.env.USERNAME+":"+process.env.PASS+"@cluster0.wveny.mongodb.net/ubisoft?retryWrites=true&w=majority"
+    mongoose.connect("mongodb+srv://prince:prince12345@cluster0.wjthg.mongodb.net/ubisoft?retryWrites=true&w=majority", {
         useFindAndModify: false,
         useNewUrlParser: true,
         useCreateIndex: true,
--- index.js
+++ index.js
@@ -33,3 +33,5 @@ app.use(errorHandler);
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server Running");
 })
+
+
